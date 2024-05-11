ba2dc2caa761db2cb82622a91fb62a772198906c
AShvab <shvabovska@gmail.com>
2024-05-11 23:48:56 +0300

    add deleting

--- src/favorite.js
+++ src/favorite.js
@@ -1,24 +1,44 @@
 import { common } from './common';
-import { createMarkup } from './helpers/createMarkup';
+import { createFavoriteMarkup } from './helpers/createMarkup';
 import { createModal } from './helpers/createModal';
 import { products } from './helpers/products';
 
 const list = document.querySelector('.js-list');
 
 const favorite = JSON.parse(localStorage.getItem(common.KEY_FAVORITE)) ?? [];
-createMarkup(favorite, list)
+createFavoriteMarkup(favorite, list);
 
-list.addEventListener('click', onClick)
+list.addEventListener('click', onClick);
 
-function onClick (event) {
-    event.preventDefault();
-    if (event.target.classList.contains('js-info')) {
-      const { id } = event.target.closest('.js-card').dataset;
-      const product = findProduct(Number(id));
-  createModal(product)
+function onClick(event) {
+  event.preventDefault();
+  if (event.target.classList.contains('js-info')) {
+    const { id } = event.target.closest('.js-card').dataset;
+    const product = findProduct(Number(id));
+    createModal(product);
+  }
 }
+
+function onDeleteClick(event) {
+  event.preventDefault();
+  if (event.target.classList.contains('js-delete')) {
+    const { id } = event.target.closest('.js-card').dataset;
+    const productId = Number(id);
+
+    const index = favorite.findIndex(item => item.id === productId);
+
+    if (index !== -1) {
+      favorite.splice(index, 1);
+
+      localStorage.setItem(common.KEY_FAVORITE, JSON.stringify(favorite));
+
+      createFavoriteMarkup(favorite, list);
+    }
+  }
 }
 
 function findProduct(productId) {
-    return products.find(({ id }) => id === productId);
-  }
+  return products.find(({ id }) => id === productId);
+}
+
+list.addEventListener('click', onDeleteClick);
--- src/helpers/createMarkup.js
+++ src/helpers/createMarkup.js
@@ -23,10 +23,40 @@ function createMarkup(arr, list) {
       .join('');
     }else{
       markup = `<li >
-      <img class="list-img" src="https://wallpapercave.com/wp/wp2013401.jpg" alt="404" width="600"/>
+      <img class="list-img" src="https://im.indiatimes.in/media/content/2017/Dec/quickmeme_1514290406_725x725.jpg" alt="404" style="width: 400px; height: auto;"/>
       </li>`
     }
     list.innerHTML = markup;
   }
 
-  export { createMarkup}
+  function createFavoriteMarkup(arr, list) {
+    let markup;
+    if(arr.length){
+      markup = arr
+        .map(
+          ({ id, img, name, price }) =>
+            `
+    <li class="list-item js-card" data-id="${id}">
+    <img class="list-img" src="${img}" alt="${name}" />
+    <h2 class="list-title">${name}</h2>
+    <p class="list-link">
+    <a href="#" class="js-info">More info</a>
+    </p>
+    <p >${price} грн.</p>
+    <div class="list-btn">
+    <button class="js-delete">Delete</button>
+    <button class="js-cart">Add to cart</button>
+    </div>
+    </li>
+    `
+        )
+        .join('');
+      }else{
+        markup = `<li >
+        <img class="list-img" src="https://im.indiatimes.in/media/content/2017/Dec/quickmeme_1514290406_725x725.jpg" alt="404" style="width: 400px; height: auto;"/>
+        </li>`
+      }
+      list.innerHTML = markup;
+    }
+
+  export { createMarkup, createFavoriteMarkup}
