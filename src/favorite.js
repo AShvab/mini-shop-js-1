ac72ff6b044d2640a5dcc6a41b1ca8f5fa0a3c8f
AShvab <shvabovska@gmail.com>
2024-05-21 23:11:52 +0300

    fixed favorite

--- src/favorite.js
+++ src/favorite.js
@@ -14,8 +14,9 @@ list.addEventListener('click', addToCartFromFavorite);
 
 function onClick(event) {
   event.preventDefault();
-  if (event.target.classList.contains('js-info')) {
-    const { id } = event.target.closest('.js-card').dataset;
+  const card = event.target.closest('.js-card');
+  if (card && event.target.classList.contains('js-info')) {
+    const { id } = card.dataset;
     const product = findProduct(Number(id));
     createModal(product);
   }
@@ -23,8 +24,9 @@ function onClick(event) {
 
 function onDeleteClick(event) {
   event.preventDefault();
-  if (event.target.classList.contains('js-delete')) {
-    const { id } = event.target.closest('.js-card').dataset;
+  const card = event.target.closest('.js-card');
+  if (card && event.target.classList.contains('js-delete')) {
+    const { id } = card.dataset;
     const productId = Number(id);
 
     const index = favorite.findIndex(item => item.id === productId);
@@ -45,22 +47,25 @@ function findProduct(productId) {
 
 function addToCartFromFavorite(event) {
   event.preventDefault();
-  const { id } = event.target.closest('.js-card').dataset;
-  const product = findProduct(Number(id));
+  const card = event.target.closest('.js-card');
+  if (card && event.target.classList.contains('js-cart')) {
+    const { id } = card.dataset;
+    const product = findProduct(Number(id));
 
-  const existingProductIndex = cart.findIndex(item => item.id === product.id);
-  if (existingProductIndex !== -1) {
-    cart[existingProductIndex].quantity++;
-    localStorage.setItem(common.KEY_CART, JSON.stringify(cart));
-  } else {
-    cart.push({ ...product, quantity: 1 });
-    localStorage.setItem(common.KEY_CART, JSON.stringify(cart));
-  }
+    const existingProductIndex = cart.findIndex(item => item.id === product.id);
+    if (existingProductIndex !== -1) {
+      cart[existingProductIndex].quantity++;
+      localStorage.setItem(common.KEY_CART, JSON.stringify(cart));
+    } else {
+      cart.push({ ...product, quantity: 1 });
+      localStorage.setItem(common.KEY_CART, JSON.stringify(cart));
+    }
 
-  const indexInFavorite = favorite.findIndex(item => item.id === product.id);
-  if (indexInFavorite !== -1) {
-    favorite.splice(indexInFavorite, 1);
-    localStorage.setItem(common.KEY_FAVORITE, JSON.stringify(favorite));
-    createFavoriteMarkup(favorite, list);
+    const indexInFavorite = favorite.findIndex(item => item.id === product.id);
+    if (indexInFavorite !== -1) {
+      favorite.splice(indexInFavorite, 1);
+      localStorage.setItem(common.KEY_FAVORITE, JSON.stringify(favorite));
+      createFavoriteMarkup(favorite, list);
+    }
   }
 }
